e85f028fd78cee31905001a2e329d673f9c0fe9c
Travis-are <cardicrane7@gmail.com>
2026-08-14 22:38:07 +0100

    Add Next.js configuration with custom headers

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/embed',
+        headers: [
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
+          { key: 'Content-Security-Policy', value: "frame-ancestors *" }
+        ],
+      },
+    ];
+  },
+}
+module.exports = nextConfig
